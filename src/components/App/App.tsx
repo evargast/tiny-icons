389ef52a3a8e439d403b41f0cba5fa0786a2a17e
Eduardo Vargas <evargast@adobe.com>
2020-09-02 15:57:33 -0500

    Tiny button demo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,31 @@
-import { Button, Provider } from "@adobe/react-spectrum";
+import { ActionButton, Provider } from "@adobe/react-spectrum";
+import ColumnSettings from "@spectrum-icons/workflow/ColumnSettings";
 import * as React from "react";
 import { FC } from "react";
 import SpectrumWhiteTheme from "spectrum-white-theme";
 
+import "./styles.css";
+
 interface AppProps {
-    text?: string;
     disabled?: boolean;
     onClick?: VoidFunction;
 }
 
-const App: FC<AppProps> = ({
-    onClick,
-    text = "Welcome to Adobe Spectrum !",
-    disabled = false,
-}) => {
+const App: FC<AppProps> = ({ onClick, disabled = false }) => {
     return (
         <Provider theme={SpectrumWhiteTheme} colorScheme="light">
-            <Button variant="cta" isDisabled={disabled} onPress={onClick}>
-                {text}
-            </Button>
+            <div className="App">
+                <h1>Hello World! This is a React app using React Spectrum!</h1>
+                <h2>Check the tiny icon below</h2>
+                <ActionButton
+                    aria-label="Column visibility popover"
+                    justifySelf="end"
+                    isDisabled={disabled}
+                    onPress={onClick}
+                >
+                    <ColumnSettings />
+                </ActionButton>
+            </div>
         </Provider>
     );
 };
